7a394061a3eb564b673008ca7402ce5270c89326
Julian <julian.kmazo@gmail.com>
2014-11-14 19:33:26 -0500

    page 67

--- app/models/article.js
+++ app/models/article.js
@@ -1,9 +1,11 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+	friend: DS.belongsTo('friend'),
   description: DS.attr('string'),
   createdAt: DS.attr('date'),
-  state: DS.attr('string'),
-  notes: DS.attr('string'),
-  friend: DS.belongsTo('friend')
+  state: DS.attr('string', {
+  	defaultValue: 'borrowed'
+  }),
+  notes: DS.attr('string')
 });

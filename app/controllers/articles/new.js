0c3835fde73c073a29d4dd11b779c2b996ff8775
Julian <julian.kmazo@gmail.com>
2014-12-04 08:29:21 -0500

    commit

---
+++ app/controllers/articles/new.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.ObjectController.extend({
+	hasDescription: Ember.computed.notEmpty('description'),
+	hasNotes: Ember.computed.notEmpty('notes'),
+	isValid: Ember.computed.and('hasDescription', 'hasNotes'),
+	actions: {
+		save: function() {
+			if (this.get('isValid')) {
+				return true;
+			}else {
+				this.set('errorMessage', 'You have to fill all the fields');
+				this.set('classError', 'has-error');
+
+				return false;
+			}
+		}
+	}
+});
